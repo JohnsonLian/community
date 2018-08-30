982cc16bcfd2ea67ebdfa3a2d25318b3a4c7309a
连云鹏 <yunpeng.lian@xiaobao100.com>
2018-08-30 17:43:55 +0800

    master:第八次提交

---
+++ WebApi - 副本/WebApiIService/Dto/SearchGoodsDto.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WebApiIService.Dto
+{
+    /// <summary>
+    /// 搜索商品模型
+    /// </summary>
+    public class SearchGoodsDto
+    {
+        /// <summary>
+        /// 搜索框的内容
+        /// </summary>
+        public string Name { get; set; }
+        /// <summary>
+        /// 标签id
+        /// </summary>
+        public int Label_id { get; set; }
+        /// <summary>
+        /// 商品状态
+        /// </summary>
+        public int State { get; set; }
+        /// <summary>
+        /// 当前页数
+        /// </summary>
+        public int Page { get; set; }
+        /// <summary>
+        /// 一页可容纳条数
+        /// </summary>
+        public int PageSize { get; set; }
+    }
+}
---
+++ WebApi - 副本/WebApiRepository/Entity/GoodsRepo.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace WebApiRepository.Entity
+{
+    /// <summary>
+    /// 商品实体类
+    /// </summary>
+    [Table("Goods")]
+    public class GoodsRepo
+    {
+        /// <summary>
+        /// 商品id
+        /// </summary>
+        public int Id { get; set; }
+        /// <summary>
+        /// 商品编号
+        /// </summary>
+        [Required, MaxLength(100)]
+        public string Number { get; set; }
+        /// <summary>
+        /// 商品名称
+        /// </summary>
+        [Required, MaxLength(100)]
+        public string Name { get; set; }
+        /// <summary>
+        /// 商品拼音
+        /// </summary>
+        public string Pinyin { get; set; }
+        /// <summary>
+        /// 商品单价
+        /// </summary>
+        public decimal Price { get; set; }
+        /// <summary>
+        /// 商品描述
+        /// </summary>
+        public string Describe { get; set; }
+        /// <summary>
+        /// 商品状态
+        /// </summary>
+        public string State { get; set; }
+        /// <summary>
+        /// 创建时间
+        /// </summary>
+        [DatabaseGenerated(DatabaseGeneratedOption.Computed)]
+        public DateTime Createtime { get; set; }
+        /// <summary>
+        /// 最近更新时间
+        /// </summary>
+        public DateTime Updatetime { get; set; }
+    }
+}
